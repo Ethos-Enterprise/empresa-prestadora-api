76c21903d17b1a306f6069f801cad0afc57e9ef9
Jefferson <jefferson.silva@jazztech.com.br>
2023-08-21 14:49:19 -0300

    adicionando projeto spring boot

---
+++ src/main/java/com/ethos/empresaprestadoraapi/EmpresaPrestadoraApiApplication.java
@@ -0,0 +1,13 @@
+package com.ethos.empresaprestadoraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaPrestadoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaPrestadoraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ethos/empresaprestadoraapi/EmpresaPrestadoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethos.empresaprestadoraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaPrestadoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
